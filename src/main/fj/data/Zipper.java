93a6d7ee886791e3f42ccc832001b841908b6ae1
runarorama <runarorama@9da73084-c327-11dd-bdf1-993be837f8bb>
2009-02-23 22:46:44 +0000

    .

--- src/main/fj/data/Zipper.java
+++ src/main/fj/data/Zipper.java
@@ -1,7 +1,5 @@
-package fj.data.zippers;
+package fj.data;
 
-import fj.data.Stream;
-import fj.data.Option;
 import static fj.data.Stream.nil;
 import static fj.data.Stream.repeat;
 import static fj.data.Option.some;
@@ -21,20 +19,18 @@ import static fj.Function.compose;
 import static fj.Function.join;
 
 /**
- * Provides a pointed stream, which is a non-empty stream structure that tracks an index (focus)
+ * Provides a pointed stream, which is a non-empty zipper-like stream structure that tracks an index (focus)
  * position in a stream. Focus can be moved forward and backwards through the stream, elements can be inserted
  * before or after the focused position, and the focused item can be deleted.
  * <p/>
  * Based on the pointedlist library by Jeff Wheeler.
- *
- * @param <A>
  */
-public class StreamZipper<A> {
+public class Zipper<A> {
   private final Stream<A> left;
   private final A focus;
   private final Stream<A> right;
 
-  private StreamZipper(final Stream<A> left, final A focus, final Stream<A> right) {
+  private Zipper(final Stream<A> left, final A focus, final Stream<A> right) {
     this.left = left;
     this.focus = focus;
     this.right = right;
@@ -49,8 +45,8 @@ public class StreamZipper<A> {
    * @param right The stream of elements after the focus.
    * @return a new StreamZipper with the given streams before and after the focus, and the given focused item.
    */
-  public static <A> StreamZipper<A> streamZipper(final Stream<A> left, final A focus, final Stream<A> right) {
-    return new StreamZipper<A>(left, focus, right);
+  public static <A> Zipper<A> zipper(final Stream<A> left, final A focus, final Stream<A> right) {
+    return new Zipper<A>(left, focus, right);
   }
 
   /**
@@ -60,8 +56,8 @@ public class StreamZipper<A> {
    *          respectively.
    * @return a new StreamZipper created from the given triple.
    */
-  public static <A> StreamZipper<A> streamZipper(final P3<Stream<A>, A, Stream<A>> p) {
-    return new StreamZipper<A>(p._1(), p._2(), p._3());
+  public static <A> Zipper<A> zipper(final P3<Stream<A>, A, Stream<A>> p) {
+    return new Zipper<A>(p._1(), p._2(), p._3());
   }
 
   /**
@@ -78,9 +74,9 @@ public class StreamZipper<A> {
    *
    * @return A first-class function that yields the product-3 representation of a given StreamZipper.
    */
-  public static <A> F<StreamZipper<A>, P3<Stream<A>, A, Stream<A>>> p_() {
-    return new F<StreamZipper<A>, P3<Stream<A>, A, Stream<A>>>() {
-      public P3<Stream<A>, A, Stream<A>> f(final StreamZipper<A> a) {
+  public static <A> F<Zipper<A>, P3<Stream<A>, A, Stream<A>>> p_() {
+    return new F<Zipper<A>, P3<Stream<A>, A, Stream<A>>>() {
+      public P3<Stream<A>, A, Stream<A>> f(final Zipper<A> a) {
         return a.p();
       }
     };
@@ -92,9 +88,9 @@ public class StreamZipper<A> {
    * @param o An <link>Ord</link> instance for the element type.
    * @return An <link>Ord</link> instance for Zippers.
    */
-  public static <A> Ord<StreamZipper<A>> ord(final Ord<A> o) {
+  public static <A> Ord<Zipper<A>> ord(final Ord<A> o) {
     final Ord<Stream<A>> so = Ord.streamOrd(o);
-    return Ord.p3Ord(so, o, so).comap(StreamZipper.<A>p_());
+    return Ord.p3Ord(so, o, so).comap(Zipper.<A>p_());
   }
 
   /**
@@ -103,9 +99,9 @@ public class StreamZipper<A> {
    * @param e An <link>Equal</link> instance for the element type.
    * @return An <link>Equal</link> instance for Zippers.
    */
-  public static <A> Equal<StreamZipper<A>> eq(final Equal<A> e) {
+  public static <A> Equal<Zipper<A>> eq(final Equal<A> e) {
     final Equal<Stream<A>> se = Equal.streamEqual(e);
-    return Equal.p3Equal(se, e, se).comap(StreamZipper.<A>p_());
+    return Equal.p3Equal(se, e, se).comap(Zipper.<A>p_());
   }
 
   /**
@@ -114,9 +110,9 @@ public class StreamZipper<A> {
    * @param s A <link>Show</link> instance for the element type.
    * @return A <link>Show</link> instance for Zippers.
    */
-  public static <A> Show<StreamZipper<A>> show(final Show<A> s) {
+  public static <A> Show<Zipper<A>> show(final Show<A> s) {
     final Show<Stream<A>> ss = Show.streamShow(s);
-    return Show.p3Show(ss, s, ss).comap(StreamZipper.<A>p_());
+    return Show.p3Show(ss, s, ss).comap(Zipper.<A>p_());
   }
 
   /**
@@ -125,8 +121,8 @@ public class StreamZipper<A> {
    * @param f A function to map across this zipper.
    * @return A new zipper with the given function applied to all elements.
    */
-  public <B> StreamZipper<B> map(final F<A, B> f) {
-    return streamZipper(left.map(f), f.f(focus), right.map(f));
+  public <B> Zipper<B> map(final F<A, B> f) {
+    return zipper(left.map(f), f.f(focus), right.map(f));
   }
 
   /**
@@ -148,8 +144,8 @@ public class StreamZipper<A> {
    * @param a The focus element of the new zipper.
    * @return a new zipper with a single element which is in focus.
    */
-  public static <A> StreamZipper<A> single(final A a) {
-    return streamZipper(Stream.<A>nil(), a, Stream.<A>nil());
+  public static <A> Zipper<A> single(final A a) {
+    return zipper(Stream.<A>nil(), a, Stream.<A>nil());
   }
 
   /**
@@ -160,11 +156,11 @@ public class StreamZipper<A> {
    * @param a The stream from which to create a zipper.
    * @return a new zipper if the provided stream has at least one element, otherwise None.
    */
-  public static <A> Option<StreamZipper<A>> fromStream(final Stream<A> a) {
+  public static <A> Option<Zipper<A>> fromStream(final Stream<A> a) {
     if (a.isEmpty())
       return none();
     else
-      return some(streamZipper(Stream.<A>nil(), a.head(), a.tail()._1()));
+      return some(zipper(Stream.<A>nil(), a.head(), a.tail()._1()));
   }
 
   /**
@@ -175,12 +171,12 @@ public class StreamZipper<A> {
    * @param a The stream from which to create a zipper.
    * @return a new zipper if the provided stream has at least one element, otherwise None.
    */
-  public static <A> Option<StreamZipper<A>> fromStreamEnd(final Stream<A> a) {
+  public static <A> Option<Zipper<A>> fromStreamEnd(final Stream<A> a) {
     if (a.isEmpty())
       return none();
     else {
       final Stream<A> xs = a.reverse();
-      return some(streamZipper(xs.tail()._1(), xs.head(), Stream.<A>nil()));
+      return some(zipper(xs.tail()._1(), xs.head(), Stream.<A>nil()));
     }
   }
 
@@ -199,8 +195,8 @@ public class StreamZipper<A> {
    * @return An optional zipper with the focus moved one element to the right, if there are elements to the right of
    *         focus, otherwise None.
    */
-  public Option<StreamZipper<A>> next() {
-    return right.isEmpty() ? Option.<StreamZipper<A>>none() : some(tryNext());
+  public Option<Zipper<A>> next() {
+    return right.isEmpty() ? Option.<Zipper<A>>none() : some(tryNext());
   }
 
   /**
@@ -209,11 +205,11 @@ public class StreamZipper<A> {
    * @return A zipper with the focus moved one element to the right, if there are elements to the right of
    *         focus, otherwise throws an error.
    */
-  public StreamZipper<A> tryNext() {
+  public Zipper<A> tryNext() {
     if (right.isEmpty())
       throw new Error("Tried next at the end of a zipper.");
     else
-      return streamZipper(left.cons(focus), right.head(), right.tail()._1());
+      return zipper(left.cons(focus), right.head(), right.tail()._1());
   }
 
   /**
@@ -222,8 +218,8 @@ public class StreamZipper<A> {
    * @return An optional zipper with the focus moved one element to the left, if there are elements to the left of
    *         focus, otherwise None.
    */
-  public Option<StreamZipper<A>> previous() {
-    return right.isEmpty() ? Option.<StreamZipper<A>>none() : some(tryPrevious());
+  public Option<Zipper<A>> previous() {
+    return right.isEmpty() ? Option.<Zipper<A>>none() : some(tryPrevious());
   }
 
   /**
@@ -232,11 +228,11 @@ public class StreamZipper<A> {
    * @return A zipper with the focus moved one element to the left, if there are elements to the left of
    *         focus, otherwise throws an error.
    */
-  public StreamZipper<A> tryPrevious() {
+  public Zipper<A> tryPrevious() {
     if (left.isEmpty())
       throw new Error("Tried previous at the beginning of a zipper.");
     else
-      return streamZipper(left.tail()._1(), left.head(), right.cons(focus));
+      return zipper(left.tail()._1(), left.head(), right.cons(focus));
   }
 
   /**
@@ -244,9 +240,9 @@ public class StreamZipper<A> {
    *
    * @return A function that moves the given zipper's focus to the next element.
    */
-  public static <A> F<StreamZipper<A>, Option<StreamZipper<A>>> next_() {
-    return new F<StreamZipper<A>, Option<StreamZipper<A>>>() {
-      public Option<StreamZipper<A>> f(final StreamZipper<A> as) {
+  public static <A> F<Zipper<A>, Option<Zipper<A>>> next_() {
+    return new F<Zipper<A>, Option<Zipper<A>>>() {
+      public Option<Zipper<A>> f(final Zipper<A> as) {
         return as.next();
       }
     };
@@ -257,9 +253,9 @@ public class StreamZipper<A> {
    *
    * @return A function that moves the given zipper's focus to the previous element.
    */
-  public static <A> F<StreamZipper<A>, Option<StreamZipper<A>>> previous_() {
-    return new F<StreamZipper<A>, Option<StreamZipper<A>>>() {
-      public Option<StreamZipper<A>> f(final StreamZipper<A> as) {
+  public static <A> F<Zipper<A>, Option<Zipper<A>>> previous_() {
+    return new F<Zipper<A>, Option<Zipper<A>>>() {
+      public Option<Zipper<A>> f(final Zipper<A> as) {
         return as.previous();
       }
     };
@@ -271,8 +267,8 @@ public class StreamZipper<A> {
    * @param a A new element to insert into this zipper.
    * @return A new zipper with the given element in focus, and the current focus element on its right.
    */
-  public StreamZipper<A> insertLeft(final A a) {
-    return streamZipper(left, a, right.cons(focus));
+  public Zipper<A> insertLeft(final A a) {
+    return zipper(left, a, right.cons(focus));
   }
 
   /**
@@ -281,8 +277,8 @@ public class StreamZipper<A> {
    * @param a A new element to insert into this zipper.
    * @return A new zipper with the given element in focus, and the current focus element on its left.
    */
-  public StreamZipper<A> insertRight(final A a) {
-    return streamZipper(left.cons(focus), a, right);
+  public Zipper<A> insertRight(final A a) {
+    return zipper(left.cons(focus), a, right);
   }
 
   /**
@@ -293,10 +289,10 @@ public class StreamZipper<A> {
    * @return A new zipper with this zipper's focus element removed, or None if deleting the focus element
    *         would cause the zipper to be empty.
    */
-  public Option<StreamZipper<A>> deleteLeft() {
+  public Option<Zipper<A>> deleteLeft() {
     return left.isEmpty() && right.isEmpty()
-        ? Option.<StreamZipper<A>>none()
-        : some(streamZipper(left, left.isEmpty() ? right.head() : left.head(), right));
+        ? Option.<Zipper<A>>none()
+        : some(zipper(left, left.isEmpty() ? right.head() : left.head(), right));
   }
 
   /**
@@ -307,10 +303,10 @@ public class StreamZipper<A> {
    * @return A new zipper with this zipper's focus element removed, or None if deleting the focus element
    *         would cause the zipper to be empty.
    */
-  public Option<StreamZipper<A>> deleteRight() {
+  public Option<Zipper<A>> deleteRight() {
     return left.isEmpty() && right.isEmpty()
-        ? Option.<StreamZipper<A>>none()
-        : some(streamZipper(left, right.isEmpty() ? left.head() : right.head(), right));
+        ? Option.<Zipper<A>>none()
+        : some(zipper(left, right.isEmpty() ? left.head() : right.head(), right));
   }
 
   /**
@@ -318,9 +314,9 @@ public class StreamZipper<A> {
    *
    * @return A new zipper with the focus element as the only element.
    */
-  public StreamZipper<A> deleteOthers() {
+  public Zipper<A> deleteOthers() {
     final Stream<A> nil = nil();
-    return streamZipper(nil, focus, nil);
+    return zipper(nil, focus, nil);
   }
 
   /**
@@ -357,21 +353,21 @@ public class StreamZipper<A> {
    * @return a zipper of variations of the provided zipper, in which each element is focused,
    *         with this zipper as the focus of the zipper of zippers.
    */
-  public StreamZipper<StreamZipper<A>> positions() {
-    final Stream<StreamZipper<A>> left = Stream.unfold(
-        new F<StreamZipper<A>, Option<P2<StreamZipper<A>, StreamZipper<A>>>>() {
-          public Option<P2<StreamZipper<A>, StreamZipper<A>>> f(final StreamZipper<A> p) {
-            return p.previous().map(join(P.<StreamZipper<A>, StreamZipper<A>>p2()));
+  public Zipper<Zipper<A>> positions() {
+    final Stream<Zipper<A>> left = Stream.unfold(
+        new F<Zipper<A>, Option<P2<Zipper<A>, Zipper<A>>>>() {
+          public Option<P2<Zipper<A>, Zipper<A>>> f(final Zipper<A> p) {
+            return p.previous().map(join(P.<Zipper<A>, Zipper<A>>p2()));
           }
         }, this);
-    final Stream<StreamZipper<A>> right = Stream.unfold(
-        new F<StreamZipper<A>, Option<P2<StreamZipper<A>, StreamZipper<A>>>>() {
-          public Option<P2<StreamZipper<A>, StreamZipper<A>>> f(final StreamZipper<A> p) {
-            return p.next().map(join(P.<StreamZipper<A>, StreamZipper<A>>p2()));
+    final Stream<Zipper<A>> right = Stream.unfold(
+        new F<Zipper<A>, Option<P2<Zipper<A>, Zipper<A>>>>() {
+          public Option<P2<Zipper<A>, Zipper<A>>> f(final Zipper<A> p) {
+            return p.next().map(join(P.<Zipper<A>, Zipper<A>>p2()));
           }
         }, this);
 
-    return streamZipper(left, this, right);
+    return zipper(left, this, right);
   }
 
   /**
@@ -380,7 +376,7 @@ public class StreamZipper<A> {
    * @param f The comonadic function to apply for each variation of this zipper.
    * @return A new zipper, with the given function applied for each variation of this zipper.
    */
-  public <B> StreamZipper<B> cobind(final F<StreamZipper<A>, B> f) {
+  public <B> Zipper<B> cobind(final F<Zipper<A>, B> f) {
     return positions().map(f);
   }
 
@@ -391,8 +387,8 @@ public class StreamZipper<A> {
    * @return A new zipper of pairs, with each element of this zipper paired with a boolean that is true if that
    *         element has focus, and false otherwise.
    */
-  public StreamZipper<P2<A, Boolean>> zipWithFocus() {
-    return streamZipper(left.zip(repeat(false)), P.p(focus, true), right.zip(repeat(false)));
+  public Zipper<P2<A, Boolean>> zipWithFocus() {
+    return zipper(left.zip(repeat(false)), P.p(focus, true), right.zip(repeat(false)));
   }
 
   /**
@@ -401,18 +397,18 @@ public class StreamZipper<A> {
    * @param n The index to which to move the focus.
    * @return A new zipper with the focus moved to the specified index, or none if there is no such index.
    */
-  public Option<StreamZipper<A>> move(final int n) {
+  public Option<Zipper<A>> move(final int n) {
     final int ll = left.length();
     final int rl = right.length();
-    Option<StreamZipper<A>> p = some(this);
+    Option<Zipper<A>> p = some(this);
     if (n < 0 || n >= length())
       return none();
     else if (ll >= n)
       for (int i = ll - n; i > 0; i--)
-        p = p.bind(StreamZipper.<A>previous_());
+        p = p.bind(Zipper.<A>previous_());
     else if (rl >= n)
       for (int i = rl - n; i > 0; i--)
-        p = p.bind(StreamZipper.<A>next_());
+        p = p.bind(Zipper.<A>next_());
     return p;
   }
 
@@ -423,11 +419,11 @@ public class StreamZipper<A> {
    * @param a An element to find in this zipper.
    * @return A new zipper with the given element focused if it is present in this zipper.
    */
-  public Option<StreamZipper<A>> find(final Equal<A> e, final A a) {
-    for (Option<StreamZipper<A>> x = some(this); x.isSome(); x = x.bind(StreamZipper.<A>previous_()))
+  public Option<Zipper<A>> find(final Equal<A> e, final A a) {
+    for (Option<Zipper<A>> x = some(this); x.isSome(); x = x.bind(Zipper.<A>previous_()))
       if (e.eq(x.some().focus(), a))
         return x;
-    for (Option<StreamZipper<A>> x = some(this); x.isSome(); x = x.bind(StreamZipper.<A>next_()))
+    for (Option<Zipper<A>> x = some(this); x.isSome(); x = x.bind(Zipper.<A>next_()))
       if (e.eq(x.some().focus(), a))
         return x;
     return none();
@@ -441,4 +437,73 @@ public class StreamZipper<A> {
   public int index() {
     return left.length();
   }
+
+  /**
+   * Move the focus to the next element. If the last element is focused, loop to the first element.
+   *
+   * @return A new zipper with the next element focused, unless the last element is currently focused, in which case
+   *         the first element becomes focused.
+   */
+  public Zipper<A> cycleNext() {
+    if (left.isEmpty() && right.isEmpty())
+      return this;
+    else if (right.isEmpty()) {
+      final Stream<A> xs = left.reverse();
+      return zipper(Stream.<A>nil(), xs.head(), xs.tail()._1().snoc(P.p(focus)));
+    } else
+      return tryNext();
+  }
+
+  /**
+   * Move the focus to the previous element. If the first element is focused, loop to the last element.
+   *
+   * @return A new zipper with the previous element focused, unless the first element is currently focused,
+   *         in which case the last element becomes focused.
+   */
+  public Zipper<A> cyclePrevious() {
+    if (left.isEmpty() && right.isEmpty())
+      return this;
+    else if (left.isEmpty()) {
+      final Stream<A> xs = right.reverse();
+      return zipper(xs.tail()._1().snoc(P.p(focus)), xs.head(), Stream.<A>nil());
+    } else
+      return tryPrevious();
+  }
+
+  /**
+   * Possibly deletes the element at the focus, then move the element on the left into focus. If no element is on the
+   * left, focus on the last element. If the deletion will cause the list to be empty, return None.
+   *
+   * @return A new zipper with the focused element removed, and focus on the previous element to the left, or the last
+   *         element if there is no element to the left.
+   */
+  public Option<Zipper<A>> deleteLeftCycle() {
+    if (left.isEmpty() && right.isEmpty())
+      return none();
+    else if (left.isNotEmpty())
+      return some(zipper(left.tail()._1(), left.head(), right));
+    else {
+      final Stream<A> xs = right.reverse();
+      return some(zipper(xs.tail()._1(), xs.head(), Stream.<A>nil()));
+    }
+  }
+
+  /**
+   * Possibly deletes the element at the focus, then move the element on the right into focus. If no element is on the
+   * right, focus on the first element. If the deletion will cause the list to be empty, return None.
+   *
+   * @return A new zipper with the focused element removed, and focus on the next element to the right, or the first
+   *         element if there is no element to the right.
+   */
+  public Option<Zipper<A>> deleteRightCycle() {
+    if (left.isEmpty() && right.isEmpty())
+      return none();
+    else if (right.isNotEmpty())
+      return some(zipper(left, right.head(), right.tail()._1()));
+    else {
+      final Stream<A> xs = left.reverse();
+      return some(zipper(Stream.<A>nil(), xs.head(), xs.tail()._1()));
+    }
+  }
+
 }
