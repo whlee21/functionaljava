6699355c130622416353759f46a78a5bf42c7188
runarorama <runarorama@9da73084-c327-11dd-bdf1-993be837f8bb>
2009-05-28 08:25:35 +0000

    Kleisli

--- trunk/etc/demo/1.5/euler/Problem2.java
+++ trunk/etc/demo/1.5/euler/Problem2.java
@@ -3,6 +3,7 @@ package euler;
 import fj.F2;
 import static fj.Function.curry;
 import fj.P1;
+import static fj.FW.$;
 import fj.data.Stream;
 import static fj.data.Stream.cons;
 import static fj.function.Integers.even;
@@ -18,7 +19,7 @@ public class Problem2 {
   public static void main(final String[] args) {
     final Stream<Integer> fibs = new F2<Integer, Integer, Stream<Integer>>() {
       public Stream<Integer> f(final Integer a, final Integer b) {
-        return cons(a, P1.curry(curry(this).f(b)).f(a + b));
+        return cons(a, $(curry(this).f(b)).lazy().f(a + b));
       }
     }.f(1, 2);
     out.println(sum(fibs.filter(even).takeWhile(intOrd.isLessThan(4000001)).toList()));
