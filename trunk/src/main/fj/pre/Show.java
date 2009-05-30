6cba54c935ad15da091fc653dbd041d4102702c4
tonymorris <tonymorris@9da73084-c327-11dd-bdf1-993be837f8bb>
2009-05-30 08:27:01 +0000

    .

--- trunk/src/main/fj/pre/Show.java
+++ trunk/src/main/fj/pre/Show.java
@@ -69,6 +69,16 @@ public final class Show<A> {
   }
 
   /**
+   * Returns the display rendering of the given argument.
+   *
+   * @param a The argument to display.
+   * @return The display rendering of the given argument.
+   */
+  public List<Character> showl(final A a) {
+    return show(a).toList();
+  }
+
+  /**
    * Returns the display rendering of the given argument as a <code>String</code>.
    *
    * @param a The argument to display.
