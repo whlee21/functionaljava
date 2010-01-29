2251d4b17542d45c1c93c0c72621f70bc1306fcc
tonymorris <tonymorris@9da73084-c327-11dd-bdf1-993be837f8bb>
2010-01-29 23:56:08 +0000

    Automated build

---
+++ artifacts/2.21/src/package-info/fj/pre/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * A prelude of abstract types that are used very often and not specific to any particular domain.
+ * 
+ * @version %build.number%<br>
+ *          <ul>
+ *          <li>$LastChangedRevision$</li>
+ *          <li>$LastChangedDate$</li>
+ *          </ul>
+ */
+package fj.pre;
