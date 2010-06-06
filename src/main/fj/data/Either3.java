437928496c189e41f4d80c213605ca4e7fde4e74
tonymorris <tonymorris@9da73084-c327-11dd-bdf1-993be837f8bb>
2010-06-06 12:41:57 +0000

    Compound disjunction

--- src/main/fj/data/Either3.java
+++ src/main/fj/data/Either3.java
@@ -60,7 +60,7 @@ public abstract class Either3<A, B, C> {
     });
   }
 
-  public final Either3<B, A, C> switch213() {
+  public final Either3<B, A, C> switchBAC() {
     return new Either3<B, A, C>() {
       public <X> X either3(final F<B, X> thiss, final F<A, X> that, final F<C, X> other) {
         return Either3.this.either3(that, thiss, other);
@@ -68,7 +68,7 @@ public abstract class Either3<A, B, C> {
     };
   }
 
-  public final Either3<C, B, A> switch321() {
+  public final Either3<C, B, A> switchCBA() {
     return new Either3<C, B, A>() {
       public <X> X either3(final F<C, X> thiss, final F<B, X> that, final F<A, X> other) {
         return Either3.this.either3(other, that, thiss);
@@ -76,7 +76,7 @@ public abstract class Either3<A, B, C> {
     };
   }
 
-  public final Either3<A, C, B> switch132() {
+  public final Either3<A, C, B> switchACB() {
     return new Either3<A, C, B>() {
       public <X> X either3(final F<A, X> thiss, final F<C, X> that, final F<B, X> other) {
         return Either3.this.either3(thiss, other, that);
@@ -84,7 +84,7 @@ public abstract class Either3<A, B, C> {
     };
   }
 
-  public final Either3<B, C, A> switch231() {
+  public final Either3<B, C, A> switchBCA() {
     return new Either3<B, C, A>() {
       public <X> X either3(final F<B, X> thiss, final F<C, X> that, final F<A, X> other) {
         return Either3.this.either3(other, thiss, that);
@@ -92,7 +92,7 @@ public abstract class Either3<A, B, C> {
     };
   }
 
-  public final Either3<C, A, B> switch312() {
+  public final Either3<C, A, B> switchCAB() {
     return new Either3<C, A, B>() {
       public <X> X either3(final F<C, X> thiss, final F<A, X> that, final F<B, X> other) {
         return Either3.this.either3(that, other, thiss);
