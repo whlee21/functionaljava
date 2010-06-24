54316272636c11c6e2e6e8bd002f3119da32498b
tonymorris <tonymorris@9da73084-c327-11dd-bdf1-993be837f8bb>
2010-06-24 12:17:21 +0000

    Automated build

---
+++ artifacts/3.1/src/package-info/fj/function/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * A prelude of commonly used first-class functions
+ *
+ * @version %build.number%<br>
+ *          <ul>
+ *          <li>$LastChangedRevision$</li>
+ *          <li>$LastChangedDate$</li>
+ *          </ul>
+ */
+package fj.function;
