9357529e90759d62c729657d0d650e63f2b93655
tonymorris <tonymorris@9da73084-c327-11dd-bdf1-993be837f8bb>
2010-06-24 11:01:41 +0000

    Inspection setting third pass

--- src/main/fj/data/fingertrees/Node.java
+++ src/main/fj/data/fingertrees/Node.java
@@ -63,7 +63,6 @@ public abstract class Node<V, A> {
     return m;
   }
 
-  @SuppressWarnings({"SameReturnValue"})
   public abstract P2<Integer, A> lookup(final F<V, Integer> o, final int i);
 
   public abstract <B> B match(final F<Node2<V, A>, B> n2, final F<Node3<V, A>, B> n3);
